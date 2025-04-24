ca8bdc9beda6f0f6a68a3fc569f01efa35676ed4
Angelo <n1xreyes@gmail.com>
2025-04-24 08:24:18 -0400

    Remove unused import

--- src/main/java/com/angelo/demo/config/ExternalServiceWebClientConfig.java
+++ src/main/java/com/angelo/demo/config/ExternalServiceWebClientConfig.java
@@ -2,8 +2,6 @@ package com.angelo.demo.config;
 
 import io.netty.channel.ChannelOption;
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
